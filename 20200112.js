4ed23d6ef9097d0ba877737342db5705fda74431
Cx <dalumatten@gmail.com>
2024-06-15 06:04:46 +0800

    absolute

--- 20200112.js
+++ 20200112.js
@@ -1,5 +1,3 @@
-// script.js
-
 let activeElement = null;
 let offsetX = 0;
 let offsetY = 0;
@@ -7,8 +5,10 @@ let offsetY = 0;
 document.addEventListener('mousedown', (e) => {
     if (e.target.classList.contains('icon')) {
         activeElement = e.target;
-        offsetX = e.clientX - activeElement.getBoundingClientRect().left;
-        offsetY = e.clientY - activeElement.getBoundingClientRect().top;
+        const rect = activeElement.getBoundingClientRect();
+        offsetX = e.clientX - rect.left;
+        offsetY = e.clientY - rect.top;
+        activeElement.classList.add('dragging'); // Add dragging class
         document.addEventListener('mousemove', dragElement);
         document.addEventListener('mouseup', dropElement);
     }
@@ -24,24 +24,9 @@ function dragElement(e) {
 
 function dropElement() {
     if (activeElement) {
+        activeElement.classList.remove('dragging'); // Remove dragging class
         document.removeEventListener('mousemove', dragElement);
         document.removeEventListener('mouseup', dropElement);
         activeElement = null;
     }
-}
-
-function getCoordinates() {
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
-    let output = '';
-
-    const icons = document.querySelectorAll('.icon');
-    icons.forEach(icon => {
-        const rect = icon.getBoundingClientRect();
-        const x = rect.left + rect.width / 2 - centerX;
-        const y = rect.top + rect.height / 2 - centerY;
-        output += `${icon.id}: (${x.toFixed(2)}, ${-y.toFixed(2)})<br>`;
-    });
-
-    document.getElementById('output').innerHTML = output;
 }
